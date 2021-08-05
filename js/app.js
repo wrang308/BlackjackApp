618289a897d0e8877df9c6b470fbfbbff265b32e
wrang308 <markus.wrang_94@hotmail.com>
2021-08-05 14:56:43 +0200

    added get total function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 const drawButton = document.getElementById("drawButton");
+const playerTotal = document.getElementById("playerTotal");
 
 const API = "https://www.deckofcardsapi.com/api/deck/";
 let deckId = "";
@@ -23,13 +24,47 @@ const drawCard = (user, count) => {
     .then(response => response.json())
     .then(data => cards = data)
     .then(cards =>  user.push(...cards.cards))
+    .then(renderTotal)
     .catch((error) => {
         console.log(error)
     });
+
+
   
 }
 
+const getTotal = (user) =>{
+    let total = 0;
+    let aces = 0;
+    
+    user.forEach(element => {
+        if(isNaN(parseInt(element.value))){
+            if(element.value !== "ACE")
+            total += 10;
+            else
+            aces++;
+        }
+        else{
+        total += parseInt(element.value);
+        }
+    });
 
+    if(aces > 0){
+    if(total + 11 + aces - 1 > 21){
+        total += aces;
+    }
+    else
+    total += 11 + aces - 1;
+    }
+
+    
+
+   return total; //user.reduce((a, b) => user.value + user.value, 0)
+}
+
+const renderTotal = () =>{
+    playerTotal.innerText = getTotal(playerHand) > 21 ? "bust" : getTotal(playerHand);
+}
 
 
 drawButton.addEventListener("click",(function(){
